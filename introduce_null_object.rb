e3a970051832dbc7a8f473023cdb1f78b23d5f68
Yusuke Miyazaki <ysk1180@gmail.com>
2020-05-10 10:19:11 +0900

    nullオブジェクトの導入のサンプルコード修正

--- introduce_null_object.rb
+++ introduce_null_object.rb
@@ -1,43 +1,12 @@
 # 筋肉量100のマッチョなトレーナーがいるゴールドジム
 # トレーナーがいないJOYFIT
 
-class Gym
-  attr_reader :name, :trainer
-
-  def initialize(name, trainer=nil)
-    @name = name
-    @trainer = trainer
-  end
-end
-
-class Trainer
-  attr_reader :name, :muscle_quantity
-
-  def initialize(name, muscle_quantity)
-    @name = name
-    @muscle_quantity = muscle_quantity
-  end
-end
-
-muscle_trainer = Trainer.new('マッチョトレーナー', 100)
-gold_gym = Gym.new('ゴールドジム', muscle_trainer)
-joyfit = Gym.new('ジョイフィット')
-
-# それぞれのジムのトレーナー情報を出したい
-gyms = [gold_gym, joyfit]
-gyms.each do |gym|
-  puts "#{gym.name}: #{gym.trainer&.name || 'トレーナー不在'} / 筋肉量: #{gym.trainer&.muscle_quantity || 0}"
-  # nilチェックが多くなってつらい
-end
-
-# ↓リファクタリング後
-
 # class Gym
 #   attr_reader :name, :trainer
 #
 #   def initialize(name, trainer=nil)
 #     @name = name
-#     @trainer = trainer || NoTrainer.new
+#     @trainer = trainer
 #   end
 # end
 #
@@ -50,16 +19,6 @@ end
 #   end
 # end
 #
-# class NoTrainer
-#   def name
-#     'トレーナー不在'
-#   end
-#
-#   def muscle_quantity
-#     0
-#   end
-# end
-#
 # muscle_trainer = Trainer.new('マッチョトレーナー', 100)
 # gold_gym = Gym.new('ゴールドジム', muscle_trainer)
 # joyfit = Gym.new('ジョイフィット')
@@ -67,5 +26,46 @@ end
 # # それぞれのジムのトレーナー情報を出したい
 # gyms = [gold_gym, joyfit]
 # gyms.each do |gym|
-#   puts "#{gym.name}: #{gym.trainer.name} / 筋肉量: #{gym.trainer.muscle_quantity}"
+#   puts "#{gym.name}: #{gym.trainer&.name || 'トレーナー不在'} / 筋肉量: #{gym.trainer&.muscle_quantity || 0}"
+#   # nilチェックが多くなってつらい
 # end
+
+# ↓リファクタリング後
+
+class Gym
+  attr_reader :name, :trainer
+
+  def initialize(name, trainer=nil)
+    @name = name
+    @trainer = trainer || NoTrainer.new
+  end
+end
+
+class Trainer
+  attr_reader :name, :muscle_quantity
+
+  def initialize(name, muscle_quantity)
+    @name = name
+    @muscle_quantity = muscle_quantity
+  end
+end
+
+class NoTrainer
+  def name
+    'トレーナー不在'
+  end
+
+  def muscle_quantity
+    0
+  end
+end
+
+muscle_trainer = Trainer.new('マッチョトレーナー', 100)
+gold_gym = Gym.new('ゴールドジム', muscle_trainer)
+joyfit = Gym.new('ジョイフィット')
+
+# それぞれのジムのトレーナー情報を出したい
+gyms = [gold_gym, joyfit]
+gyms.each do |gym|
+  puts "#{gym.name}: #{gym.trainer.name} / 筋肉量: #{gym.trainer.muscle_quantity}"
+end
